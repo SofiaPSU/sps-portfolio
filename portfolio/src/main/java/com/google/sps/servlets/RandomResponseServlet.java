d209591c3927d478803c9f243f8292871ff96817
SofiaPSU <sofia.riley30@gmail.com>
2021-03-12 07:20:03 +0000

    Make changes to index, servlet, script, and style files

---
+++ portfolio/src/main/java/com/google/sps/servlets/RandomResponseServlet.java
@@ -0,0 +1,30 @@
+package com.google.sps.servlets;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+
+
+@WebServlet("/random_response")
+public class RandomResponseServlet extends HttpServlet {
+    String[] phrases = {"Today is a good day!","It works!","Hi"};
+    
+  @Override
+  public void doGet(final HttpServletRequest request, final HttpServletResponse response) throws IOException {
+        final Gson gson = new Gson();
+        final String json = gson.toJson(phrases);
+    
+    response.setContentType("application/json;");
+    response.getWriter().println(json);
+    
+  }
+
+
+
+}
--- portfolio/src/main/webapp/fetchScript.js
+++ portfolio/src/main/webapp/fetchScript.js
@@ -1,10 +1,7 @@
 async function showPhrase() {
   const responseFromServer = await fetch('/step_one_servlet');
-
   const jsonFromResponse = await responseFromServer.json();
-
-  const jsonString = jsonFromResponse.phrases[Math.floor(Math.random() * jsonFromResponse.phrases.length)];
-
+  const jsonString = jsonFromResponse[Math.floor(Math.random() * jsonFromResponse.length)];
   const jsonContainer = document.getElementById('json-container');
   jsonContainer.innerText = jsonString;
 }
